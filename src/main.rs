ccd277d0c2ed78cfb31ed052bdb0b5a5a6874eaf
Nell Shamrell <nellshamrell@gmail.com>
2017-10-15 14:26:23 -0700

    basic traits
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+/*
 #[derive(Debug)]
 struct Character {
     name: String,
@@ -8,8 +9,40 @@ struct Character {
     wisdom: u8,
     charisma: u8
 }
+*/
+
+pub trait Constitution {
+    fn constitution_bonus(&self) -> u8;
+}
+
+struct Dwarf {
+   name: String
+}
+
+impl Constitution for Dwarf {
+    fn constitution_bonus(&self) -> u8 {
+        2
+    }
+}
+
+struct Elf {
+    name: String
+}
+
+impl Constitution for Elf {
+    fn constitution_bonus(&self) -> u8 {
+        0
+    }
+
+}
 
 fn main() {
+    let my_dwarf = Dwarf { name: String::from("Nell Grimly") };
+    println!("Here is the constitution bonus for Nell Grimly, the dwarf: {}", my_dwarf.constitution_bonus());
+
+    let my_elf = Elf { name: String::from("Nell Lightfooted") };
+    println!("Here is the constitution bonus for Nell Lightfooted, the elf: {}", my_elf.constitution_bonus());
+/*
     let my_character = Character {
         name: String::from("Rian Everstar"),
         strength: 8,
@@ -19,6 +52,6 @@ fn main() {
         wisdom: 5,
         charisma: 7
     };
-
     println!("Here is my character {:?}", my_character)
+*/
 }
